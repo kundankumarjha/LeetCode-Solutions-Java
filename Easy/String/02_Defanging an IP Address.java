837470124cafc4ed29a9c2ed9f6802e7be725366
Rane <vinod_rane@nuance.com>
2020-04-13 15:13:45 +0530

    Adding file

---
+++ Easy/String/02_Defanging an IP Address.java
@@ -0,0 +1,20 @@
+/* 	Given a valid (IPv4) IP address, return a defanged version of that IP address.
+A defanged IP address replaces every period "." with "[.]".
+Input: address = "1.1.1.1"
+Output: "1[.]1[.]1[.]1"
+*/
+
+class Solution {
+	public String defangIPaddr(String address) {
+		StringBuffer sb = new StringBuffer();
+
+		for (Character ch : address.toCharArray()) {
+			if (ch == '.')
+				sb.append("[.]");
+			else
+				sb.append(ch);
+		}
+		return sb.toString();
+	}
+}
+
