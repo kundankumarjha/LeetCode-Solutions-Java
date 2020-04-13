d39d206b66343e9a146bbdb56611e0e640d26b68
Rane <vinod_rane@nuance.com>
2020-04-13 14:56:07 +0530

    Added files

---
+++ Easy/String/RemoveVowelsFromAString.java
@@ -0,0 +1,20 @@
+/* Given a string S, remove the vowels 'a', 'e', 'i', 'o', and 'u' from it, and return the new string. */
+package easy.string;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+
+	public static String removeVowels(String s) {
+		StringBuilder sb = new StringBuilder();
+		Set<Character> set = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
+
+		for (Character ch : s.toCharArray()) {
+			if (!(set.contains(ch)))
+				sb.append(ch);
+		}
+		return sb.toString();
+	}
+}
